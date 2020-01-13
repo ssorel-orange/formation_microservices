08d09891fb5a06c2e2e2380cd0d5e267819c3e38
ssorel-orange <stephane.sorel@orange.com>
2020-01-13 16:25:31 -0400

    Added Test Classes

---
+++ src/main/java/com/orange/projetdemo/domain/Logement.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.orange.projetdemo.domain;
+
+/**
+ * @author steph
+ *
+ */
+public class Logement {
+
+	
+	private String nom;
+	
+	private int nbPieces;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getNbPieces() {
+		return nbPieces;
+	}
+
+	public void setNbPieces(int nbPieces) {
+		this.nbPieces = nbPieces;
+	}
+	
+	
+	
+	
+}
--- src/test/java/com/orange/projetdemo/ProjetdemoApplicationTests.java
+++ src/test/java/com/orange/projetdemo/ProjetdemoApplicationTests.java
@@ -9,5 +9,9 @@ class ProjetdemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void logementTest () {
+		
+	}
 
 }
