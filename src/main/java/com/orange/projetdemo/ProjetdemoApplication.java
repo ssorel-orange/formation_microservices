f38f8826d75cc5a9be4db5c93c7a2f4a09c8b6f1
ssorel-orange <stephane.sorel@orange.com>
2020-01-13 16:01:41 -0400

    Project intialisation

---
+++ src/main/java/com/orange/projetdemo/ProjetdemoApplication.java
@@ -0,0 +1,13 @@
+package com.orange.projetdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/projetdemo/ProjetdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.projetdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
